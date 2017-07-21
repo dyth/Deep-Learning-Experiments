574094d800caf257ef33099458614d2959db61b5
David Hui <dyth2@cam.ac.uk>
2017-07-22 00:10:14 +0100

    more cuts in multilayerPerceptron

--- multilayerPerceptron.py
+++ multilayerPerceptron.py
@@ -10,22 +10,22 @@ import numpy as np
 
 
 # variables
-dims, samples = 2, 2000
+dims, samples = 2, 1000
 
 
 # generate training and testing data and their labels
 (train_data, train_labels), (test_data, test_labels) = createData(dims, samples)
-train_labels = to_categorical(train_labels)
-test_labels = to_categorical(test_labels)
+train_data = train_data / 2.0
+test_data = test_data / 2.0
 
 
 # create model
 np.random.seed(1729)
 model = Sequential()
 model.add(Dense(units=2, input_dim=dims, activation='relu'))
-model.add(Dense(units=2, activation='relu'))
-model.add(Dense(units=2, activation='sigmoid'))
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.add(Dense(units=2**dims, activation='relu'))
+model.add(Dense(units=1, activation='sigmoid'))
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 
 # train model
